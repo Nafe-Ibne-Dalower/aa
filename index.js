112a99fc9d2b90d7dad0ccc159e6de3c662953bd
Nafe-Ibne-Dalower <ktprodhan@gmail.com>
2023-02-21 07:35:24 +0600

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const path = require("path")
 
 app.use(bodyParser.urlencoded({extended: true}));
 dir = 'awqndjqwdddddddqwddddwqqqqqqqq'
-fs.appendFileSync("data.txt", dir)
+fs.writeFile('data.txt', `module.exports = '${dir}'`, (err) => {
+  if (err) throw err;
+  console.log('Build time file created successfully!');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
